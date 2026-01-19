1e2b47813c3e64cdbab2cc9f642f81ef5aae0659
FOLKVORD <folkvord.kristoffer@gmail.com>
2026-01-19 13:55:16 +0100

    Init

---
+++ src/main/java/no/bachelor26/bachelor26/Bachelor26Application.java
@@ -0,0 +1,13 @@
+package no.bachelor26.bachelor26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bachelor26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bachelor26Application.class, args);
+	}
+
+}
---
+++ src/test/java/no/bachelor26/bachelor26/Bachelor26ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.bachelor26.bachelor26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bachelor26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

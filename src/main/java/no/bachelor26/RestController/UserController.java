fce4eee4abd3f7ac7773cde69fa41516e5e95782
FOLKVORD <deez@nutz.gotem>
2026-01-26 18:58:29 +0100

    RegistreringsAPI

---
+++ src/main/java/no/bachelor26/RestController/UserController.java
@@ -0,0 +1,37 @@
+package no.bachelor26.RestController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import no.bachelor26.DTO.RegisterUserDTO;
+
+/* 
+ * Controlleren for all brukerrelaterte API-er
+ * 
+ * For at API-kallet skal fungere som forventet, kreves det 
+ * at JSON-objektet som sendes har alle de samme attributtene
+ * som objektet i parameteret. 
+ */
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    //@Autowired
+    //UserService userService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> postRegister(@RequestBody RegisterUserDTO registrationFormDTO){
+        
+        // Kristoffer: Sofies ansvar!!!
+        //userService.register(registrationFormDTO);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+}

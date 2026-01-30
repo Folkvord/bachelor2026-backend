8d54b392d2f9deed5f5d782557244e3255b87f2e
sofieberglundd <sofie.-.berglund@hotmail.com>
2026-01-30 17:16:44 +0100

    lagt til rolle, og lengde constraints på datafelt

--- src/main/java/no/bachelor26/Entity/User.java
+++ src/main/java/no/bachelor26/Entity/User.java
@@ -18,19 +18,20 @@ public class User {
     @Column(columnDefinition = "uuid")
     private UUID id;
 
-    // SOFIE: brukernavn kan ikke være tom, må være unik
-    @Column(nullable=false,unique=true)
+    // SOFIE: brukernavn kan ikke være tom, må være unik, makslengde 25 tegn
+    @Column(nullable=false,unique=true, length = 25)
     private String username; 
 
-    // SOFIE: email kan ikke være tom, må være unik
-    @Column(nullable=false,unique=true)
+    // SOFIE: email kan ikke være tom, må være unik, makslengde 100 tegn
+    @Column(nullable=false,unique=true, length = 100)
     private String email;
 
-    // SOFIE: passord lagres i hash, og kan ikke være null
-    @Column(nullable=false)
+    // SOFIE: passord lagres i hashformet, og kan ikke være null, 
+    // og makslengde på 100 tegn
+    @Column(nullable=false, length = 100)
     private String passwordHash;
 
-    // SOFIE: rolle kan ikke oppdateres, kan ikke være tom
+    // SOFIE: rolle kan ikke oppdateres (sikkerhetsaspekt), kan ikke være tom
     @Enumerated(EnumType.STRING)
     @Column(nullable=false, updatable=false)
     private Role role;
@@ -40,7 +41,8 @@ public class User {
 
     public enum Role {
         STUDENT,
-        ADMIN
+        ADMIN,
+        DEV
     }
     
 

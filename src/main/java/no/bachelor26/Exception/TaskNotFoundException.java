487fe8873d4185b6013b5a85626f2bf9b293a35b
FOLKVORD <deez@nutz.gotem>
2026-03-05 23:02:20 +0100

    Skrivefeil

--- src/main/java/no/bachelor26/Exception/TaskNotFoundException.java
+++ src/main/java/no/bachelor26/Exception/TaskNotFoundException.java
@@ -3,7 +3,7 @@ package no.bachelor26.Exception;
 public class TaskNotFoundException extends RuntimeException {
 
     public TaskNotFoundException(String taskName){
-        super("Oppgaven med navn: '" + taskName + "', kunne ikke finnes.");
+        super("Oppgaven med navn: '" + taskName + "', finnes ikke.");
     }
 
 }

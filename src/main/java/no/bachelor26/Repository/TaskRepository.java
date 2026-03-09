61c57da5713a4ee1a32c53683384594badfb1539
FOLKVORD <deez@nutz.gotem>
2026-03-09 23:55:40 +0100

    hent BARE oppgaveinfo

--- src/main/java/no/bachelor26/Repository/TaskRepository.java
+++ src/main/java/no/bachelor26/Repository/TaskRepository.java
@@ -3,11 +3,20 @@ package no.bachelor26.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import no.bachelor26.Entity.Task;
+import no.bachelor26.Projection.TaskInfo;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
 
     Optional<Task> findByName(String name);
+
+    @Query("""
+    SELECT t.id AS id, t.name AS name, t.description AS description
+    FROM Task t
+    WHERE t.id = :id        
+    """)
+    Optional<TaskInfo> findInfoById(Long id);
 
 }

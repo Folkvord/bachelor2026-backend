7b8b9ff1533dad7e0188d61ddf509ce06f44aeb2
FOLKVORD <deez@nutz.gotem>
2026-03-09 22:41:32 +0100

    Generationstrategy

--- src/main/java/no/bachelor26/Entity/Task.java
+++ src/main/java/no/bachelor26/Entity/Task.java
@@ -3,6 +3,8 @@ package no.bachelor26.Entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -13,6 +15,7 @@ import lombok.Data;
 public class Task {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;

4e574b1188ad387962c41812b87430274c49f9ac
FOLKVORD <deez@nutz.gotem>
2026-03-10 16:17:47 +0100

    websocket oppgaveAPI

--- src/main/java/no/bachelor26/WebSocket/GameHandler.java
+++ src/main/java/no/bachelor26/WebSocket/GameHandler.java
@@ -1,15 +1,24 @@
 package no.bachelor26.WebSocket;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import no.bachelor26.Exception.NoTaskAccessException;
+import no.bachelor26.Projection.TaskInfo;
+import no.bachelor26.Service.AvailableTaskService;
+import no.bachelor26.Service.TaskService;
 import no.bachelor26.WebSocket.Messages.ClientMessages.ClientMessage;
+import no.bachelor26.WebSocket.Messages.ClientMessages.RequestTaskMessage;
 import tools.jackson.databind.ObjectMapper;
 
 
@@ -20,6 +29,14 @@ public class GameHandler extends TextWebSocketHandler {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Autowired
+    AvailableTaskService availableTaskService;
+
+    @Autowired
+    TaskService taskService;
+
+    private final String ERRORMSG = "Unknown error";
 
 
     @Override
@@ -35,24 +52,21 @@ public class GameHandler extends TextWebSocketHandler {
     
 
     @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> msg){
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> msg) throws Exception{
 
         log.info("Klient sendte medling");
 
-        // msg burde inneholde en JSON som forklarer hva brukeren vil ha
-        //String payload = msg.getPayload().toString();
-        
         String payload = msg.getPayload().toString();
         ClientMessage clientMessage = objectMapper.readValue(payload, ClientMessage.class);
 
         switch(clientMessage.getType()){
 
             case "task-info":
-                
+                respondToTaskInfo(session);
                 break;
 
             case "task":
-
+                respondToTaskRequest(session, clientMessage);
                 break;
 
             case "validate-flag":
@@ -61,9 +75,57 @@ public class GameHandler extends TextWebSocketHandler {
 
             default:
                 log.warn("wtf STYGG melding incoming");
+                session.sendMessage(new TextMessage(ERRORMSG));
                 break;
 
         }
+
+    }
+
+
+    /**
+     * Sender klienten en liste med informasjonen over de tilgjengelige opppgavene.
+     * 
+     * @param session WebSocket-sesjonsobjekt
+     * @throws Exception
+     * @author Kristoffer Folkvord
+     */
+    private void respondToTaskInfo(WebSocketSession session) throws Exception{
+        
+        UUID userID = (UUID) session.getAttributes().get("userID");
+        List<TaskInfo> taskInfo = availableTaskService.getAvailableTaskInfo(userID);
+        
+        session.sendMessage(new TextMessage(
+            objectMapper.writeValueAsBytes(
+                taskInfo
+            )
+        ));
+
+    }
+
+
+    /**
+     * Sender klienten oppgaven dersom brukeren har tilgang.
+     * 
+     * @param session WebSocket-sesjonsobjekt
+     * @param clientMessage Klientmeldingen
+     * @throws Exception
+     * @author Kristoffer Folkvord
+     */
+    private void respondToTaskRequest(WebSocketSession session, ClientMessage clientMessage) throws Exception{
+
+        UUID userID = (UUID) session.getAttributes().get("userID");
+        Long taskID = ((RequestTaskMessage) clientMessage).getTaskID();
+        
+        if(!availableTaskService.userHasAccessToTask(userID, taskID)){
+            throw new NoTaskAccessException(userID.toString(), taskID.toString());
+        }
+
+        session.sendMessage(new TextMessage(
+            objectMapper.writeValueAsBytes(
+                taskService.getTaskContentById(taskID)
+            )
+        ));
 
     }
 
--- src/main/java/no/bachelor26/WebSocket/Messages/ServerMessages/TaskInfoMessage.java
+++ src/main/java/no/bachelor26/WebSocket/Messages/ServerMessages/TaskInfoMessage.java
@@ -1,22 +1,21 @@
 package no.bachelor26.WebSocket.Messages.ServerMessages;
 
 
-import java.util.Map;
+import java.util.List;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import no.bachelor26.Projection.TaskInfo;
 import no.bachelor26.WebSocket.Messages.ClientMessages.ClientMessage;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class TaskInfoMessage extends ClientMessage {
 
-    private Map<Long, TaskInfo> info;
+    private List<TaskInfo> taskInfo;
 
-    @Data
-    public static class TaskInfo {
-        private String name;
-        private String description;
+    public TaskInfoMessage(List<TaskInfo> taskInfo){
+        this.taskInfo = taskInfo;
     }
 
 }

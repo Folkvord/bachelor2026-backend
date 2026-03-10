54c86c0106d3b80bdb023b6430105bb77d7b8438
FOLKVORD <deez@nutz.gotem>
2026-03-10 23:18:53 +0100

    Registrerings API

--- src/main/java/no/bachelor26/RestController/UserController.java
+++ src/main/java/no/bachelor26/RestController/UserController.java
@@ -1,39 +1,51 @@
 package no.bachelor26.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import no.bachelor26.DTO.RegisterUserDTO;
+import no.bachelor26.Service.UserService;
 
-/* 
- * Controlleren for all brukerrelaterte API-er
- * 
- * For at API-kallet skal fungere som forventet, kreves det 
- * at JSON-objektet som sendes har alle de samme attributtene
- * som objektet i parameteret. 
- * 
- * !!! RETURNERER 501 RN, ENDRE ETTER IMPLEMENTASJON !!!
- */
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
-    //@Autowired
-    //UserService userService;
+    @Autowired
+    UserService userService;
+
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(){
+
+        
+
+        return ResponseEntity.status(501).build();
+    }
 
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterUserDTO registrationFormDTO){
-        
-        // Kristoffer: Sofies ansvar!!!
-        //userService.register(registrationFormDTO);
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterUserDTO registrationFormDTO){
 
-        // Kristoffer: Endre ettersom implementert
-        return ResponseEntity.status(501).build();
+        log.info("LAGER BRUKER");
+
+        userService.registerUser(
+            registrationFormDTO.getUsername(),
+            registrationFormDTO.getEmail(),
+            registrationFormDTO.getPassword(),
+            registrationFormDTO.getPasswordConfirm()
+        );
+
+        return ResponseEntity.status(201).build();
 
     }
 

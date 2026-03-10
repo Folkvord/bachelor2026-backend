7a8bfcf94fe2feb4425d22d78b4f713e8561ef56
FOLKVORD <deez@nutz.gotem>
2026-03-10 16:16:57 +0100

    metoder tar nå userID istedet for user

--- src/main/java/no/bachelor26/Service/AvailableTaskService.java
+++ src/main/java/no/bachelor26/Service/AvailableTaskService.java
@@ -1,11 +1,11 @@
 package no.bachelor26.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import no.bachelor26.Entity.User;
 import no.bachelor26.Projection.TaskInfo;
 import no.bachelor26.Repository.AvailableTaskRepository;
 
@@ -19,23 +19,25 @@ public class AvailableTaskService {
     /**
      * Sjekker om en bruker har tilgang til en oppgave.
      * 
-     * @param user Brukeren
+     * @param userID Brukeren
      * @param taskID ID-en til oppgaven
      * @return 
      */
-    public boolean userHasAccessToTask(User user, Long taskID){
-        return availableTaskRepo.existsByUserIdAndTaskId(user.getId(), taskID);
+    public boolean userHasAccessToTask(UUID userID, Long taskID){
+        return availableTaskRepo.existsByUserIdAndTaskId(userID, taskID);
     }
+
 
     /**
      * Gir en liste med id-en, navnet og beskrivelsen til 
      * alle tilgjengelige oppgaver.
      * 
-     * @param user Brukeren
+     * @param userID Brukerens ID
      * @return Liste med oppgaveinfo
      */
-    public List<TaskInfo> getAvailableTaskInfo(User user){
-        return availableTaskRepo.findInfoByUserId(user.getId());
+    public List<TaskInfo> getAvailableTaskInfo(UUID userID){
+        return availableTaskRepo.findInfoByUserId(userID);
     }
+
 
 }

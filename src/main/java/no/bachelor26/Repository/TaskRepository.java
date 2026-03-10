72070a3ee8a7e4cd89a557eeb6b50d0bc9732eaa
FOLKVORD <deez@nutz.gotem>
2026-03-10 16:00:41 +0100

    Hent BARE json fra tabell

--- src/main/java/no/bachelor26/Repository/TaskRepository.java
+++ src/main/java/no/bachelor26/Repository/TaskRepository.java
@@ -13,10 +13,18 @@ public interface TaskRepository extends JpaRepository<Task, Long>{
     Optional<Task> findByName(String name);
 
     @Query("""
-    SELECT t.id AS id, t.name AS name, t.description AS description
-    FROM Task t
-    WHERE t.id = :id        
+        SELECT t.id AS id, t.name AS name, t.description AS description
+        FROM Task t
+        WHERE t.id = :id        
     """)
     Optional<TaskInfo> findInfoById(Long id);
+
+    @Query("""
+        SELECT t.task as task
+        FROM Task t
+        WHERE t.id = :id        
+    """)
+    Optional<String> findTaskContentById(Long id);
+
 
 }

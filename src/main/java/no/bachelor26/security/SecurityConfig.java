14f8400bcb6a153025ab4ff7c69a4b0a5f0a3084
FOLKVORD <deez@nutz.gotem>
2026-03-14 13:33:14 +0100

    Fjerna dupa passordhasher

--- src/main/java/no/bachelor26/security/SecurityConfig.java
+++ src/main/java/no/bachelor26/security/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -35,12 +33,6 @@ public class SecurityConfig {
                 // midlertidig enkel autentisering
                 .httpBasic(httpBasic -> {});
         return http.build();
-    }
-
-
-    @Bean
-    public PasswordEncoder Encoder(){
-        return new BCryptPasswordEncoder();
     }
 
 }

f91e103d15c87ba1cc39d773c49285ffc172ad8f
FOLKVORD <deez@nutz.gotem>
2026-03-23 16:48:04 +0100

    Brukersesjonsobjekt

---
+++ src/main/java/no/bachelor26/WebSocket/UserSession.java
@@ -0,0 +1,21 @@
+package no.bachelor26.WebSocket;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class UserSession {
+    
+    private UserState state = UserState.IDLE;
+    private UUID userID;
+
+    public UserSession(UUID userID){
+        this.userID = userID;
+    }
+
+    public String toString(){
+        return "UserID: (" + userID + "), State: (" + state + ")";
+    }
+
+}
---
+++ src/main/java/no/bachelor26/WebSocket/UserState.java
@@ -0,0 +1,7 @@
+package no.bachelor26.WebSocket;
+
+public enum UserState {
+    IDLE,
+    PARSE_STANDBY,
+    ACTIVE_TASK
+}

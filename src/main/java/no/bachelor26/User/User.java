e56260aa2cbc030ee97eac799c2e1db969986352
FOLKVORD <deez@nutz.gotem>
2026-04-25 19:07:42 +0200

    mer riktig defaultrolle

--- src/main/java/no/bachelor26/User/User.java
+++ src/main/java/no/bachelor26/User/User.java
@@ -34,7 +34,7 @@ public class User {
     // SOFIE: rolle kan ikke oppdateres (sikkerhetsaspekt), kan ikke være tom
     @Enumerated(EnumType.STRING)
     @Column(nullable=false, updatable=false)
-    private Role role = Role.DEV;
+    private Role role = Role.STUDENT;
     
     @Column(nullable = false, updatable = false)
     private Instant createdAt = Instant.now();
